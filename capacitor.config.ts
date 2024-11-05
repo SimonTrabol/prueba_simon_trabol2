220fe9b75569b3ef9b626568a9d24abdc4d943aa
SimonTrabol <simontrabol.st@gmail.com>
2024-11-05 19:30:59 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Simon_Trabol_007V',
+  webDir: 'www'
+};
+
+export default config;
